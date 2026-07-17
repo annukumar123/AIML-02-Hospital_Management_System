9934eff003963dd7e0562984b35eaee4d79ac67d
AnnuKumar <annu83175@gmail.com>
2026-07-17 10:49:49 +0530

    Clean: Deleted duplicate backend files from the frontend folder

--- hospital-frontend/src/main/java/com/hospital/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.hospital.model.Appointment;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
